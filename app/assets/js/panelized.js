b2634f60187806d93f55c608d64eccb07049d999
Matthew Simo <matthew.a.simo@gmail.com>
2013-01-02 22:58:47 -0600

    Update panelized plugin, add fittext & lettering plugins, update index scripts & main.js

--- app/assets/js/panelized.js
+++ app/assets/js/panelized.js
@@ -9,51 +9,63 @@ var methods = {
 
       var $this = $(this),
           data = $().data('panelized'),
-          panelized = $('<div />', {
-            text: $this.attr('title')
-          }),
           docheight = $(document).height(),
           winheight = $(window).height(),
           slideCollection = $this.children(),
-          hash = $().panelized('getHash');
-
-      // Set current panel
-      if(!hash)
-        $().panelized('setCurrentPanel', 0);
-      else {
-        $.each(slideCollection, function(index, value) {
-          if(hash == $(value).attr('id'))
-            $().panelized('setCurrentPanel', index);
-        });
-      }
+          hash = $().panelized('getHash'),
+          currentPanel = 0;
 
       // Listener for hash change
       window.onhashchange = function(event) {
-        console.log("onhashchange: location: " + document.location.href);
-      };
-      
+        var panelizedObj, currentPanel;
+        panelizedObj = $('#main-content').data('panelized');
+        currentPanel = $().panelized('calculatePanel');
 
-      if(!data){ // More setup here
+        if( panelizedObj.currentPanel !== currentPanel){
+          $().panelized('scrollToItem', currentPanel);
+        }
+      };
+
+      if(!data){ 
         
         $(this).data('panelized', {
           target: $this,
-          panelized: panelized,
+          slideCollection: slideCollection,
           docHeight: docheight,
           winHeight: winheight,
-          hash: hash
+          currentPanel: 0,
         });
 
+      }
+
+      // start up set current panel
+      if(hash){
+        $().panelized('scrollToItem', $().panelized('calculatePanel'));
       }
 
     });
 
   },
+
+  calculatePanel: function() {
+    var panelizedObj = $('#main-content').data('panelized');
+
+    $.each(panelizedObj.slideCollection, function(index, value) {
+      if($().panelized('getHash') == $(value).attr('id'))
+        currentPanel = index;
+    });
+    return currentPanel;
+  },
+
   getCurrentPanel: function() {
-    return false;
+    var panelizedObj = $('#main-content').data('panelized');
+    return panelizedObj.currentPanel;
   },
 
   setCurrentPanel: function(panel) {
-    console.log("current panel set to: " + panel);
+    var panelizedObj = $('#main-content').data('panelized');
+    panelizedObj.currentPanel = panel;
+    $('#main-content').data('panelized', panelizedObj);
   },
 
   isMobile: function() {
@@ -62,27 +74,45 @@ var methods = {
 
   getHash: function() {
     var hash = window.location.hash.replace('#/', '');
-    console.log("Hash start - " + hash);
-    var item = hash;
 
-    if(item != null)
-      return item;
+    if(hash != null)
+      return hash;
     return '';
   },
 
   setHash: function(id) {
     window.location.hash = "/" + id;
-  },
-
-  onHashChange: function() {
-    var target = $().panelized('getHash');
-    console.log("Hash change - " + target);
-    if(target === '') return;
     
-    $().panelized('scrollToItem', target);
+    var panelizedObj = $('#main-content').data('panelized');
+    panelizedObj.hash = id;
+    $('#main-content').data('panelized', panelizedObj);
   },
 
   scrollToItem: function(item) {
+    var currentPanel = $().panelized('getCurrentPanel');
+
+    if(item > currentPanel){
+
+      $("#main-content > div").eq(currentPanel).css('z-index', 6);
+      $("#main-content > div").eq(item).css({'top': "0%", 'z-index': 5}).show();
+      $("#main-content > div").eq(currentPanel).animate({'top': "-100%"},1000, function(){
+        $("#main-content > div").eq(currentPanel).hide().css('bottom', 0).css('z-index', 1);
+      });
+
+    } else if (item < currentPanel){
+
+      $("#main-content > div").eq(item).css({'top': "-100%", 'z-index': 6}).show();
+      $("#main-content > div").eq(item).animate({'top': "0%"},1000, function() {
+        $("#main-content > div").eq(currentPanel).css({'top': "0%", 'z-index': 1}).hide();
+        $("#main-content > div").eq(item).css('z-index', 5);
+      });
+
+    } else {
+      return false;
+    }
+
+    // Update new currentPanel
+    $().panelized('setCurrentPanel', item);
 
   },
 
@@ -92,17 +122,11 @@ var methods = {
 
   scrollToPrev: function() {
     $().panelized('scrollToItem', $().panelized('getCurrentPanel') - 1);
-  }
+  },
+
 };
 
 $.fn.panelized = function(method) {
-
-
-  // Initialize
-//  var winHeight, currentSlide;
-//  var panelCollection = this.children();
-//  var panelCount = panelCollection.size();
-
 
   if(methods[ method ]){
     return methods[ method ].apply(this, Array.prototype.slice.call( arguments, 1));
@@ -111,16 +135,6 @@ $.fn.panelized = function(method) {
   } else {
     $.error('Method ' + method + ' does not exist on jQuery.panelized');
   }
-
-//  if(!target){ // If NO target
-//
-//    console.log("no target");
-//
-//  } else { // target exists
-//
-//    console.log("target");
-//
-//  }
 
 };
 

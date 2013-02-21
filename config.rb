ec2c1ff5c95cfb339d7ac465e77f07fffa74c200
Matthew Simo <matthew.a.simo@gmail.com>
2013-02-20 19:20:10 -0600

    Turn on compressed sass compiling

--- config.rb
+++ config.rb
@@ -4,7 +4,7 @@ sass_dir = "app/assets/scss"
 images_dir = "app/assets/img"
 javascripts_dir = "app/assets/js"
 
-output_style = :expanded # nested | expanded | compact | compressed
+output_style = :compressed # nested | expanded | compact | compressed
 relative_assets = true
 line_comments = true
 
